8bc0b0fa7b311b2658dfda62c846f7ca780ee661
Denis Nobre <denis@denisnobre.com>
2022-06-23 09:04:51 -0300

    up

--- noobres.js
+++ noobres.js
@@ -2,10 +2,13 @@ module.exports = function(RED) {
     function noobres(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        const n_a = config.na;
+        const n_b = config.nb;
+
         node.on('input', function(msg) {
             msg.payload = msg.payload.toLowerCase();
-            msg.topic = soma(111,222)
-            node.send(msg);
+            msg.topic = soma(111,222) || 0
+            node.send(msg); 
         });
     }
 	
